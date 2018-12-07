a3ee658ecdc86d2adea202a4325fd23ce820cb4b
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-07 11:37:27 +0100

    Book class created

---
+++ BookShop/ClassLibrary/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Book
+    {
+        protected string title;
+        protected string author;
+        protected decimal price;
+        protected string description;
+
+        public Book()
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+            Description = description;
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+        public virtual decimal Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}

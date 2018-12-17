22835f9ed2d116b7352cdbca3f924cb6104b399a
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-17 14:36:22 +0100

    more tests

---
+++ MankindExerciseOOP/ClassLib/StringManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class StringManager
+    {
+        private const char Separator = ' ';
+        private string incomingString;
+
+        public string IncomingString
+        {
+            set => incomingString = value;
+        }
+
+        public StringManager(string incomingString)
+        {
+            IncomingString = incomingString;
+        }
+
+        public string[] SplitString()
+        {
+            return incomingString.Split(Separator);
+        }
+    }
+}
---
+++ MankindExerciseOOP/UnitTests/InputSplitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ClassLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class InputSplitTests
+    {
+        [TestMethod]
+        public void ChecktDieAnzahlDerStringsNachDemSplitStudent()
+        {
+            //Arrange
+            string inputString = "Berthram Bobsen 36489";
+            int arrayCountExpected = 3;
+            
+            //Act
+            var stringManager = new StringManager(inputString);
+
+            //Assert
+            Assert.AreEqual(arrayCountExpected, stringManager.SplitString().Length);
+        }
+
+        [TestMethod]
+        public void ChecktDieAnzahlDerStringsNachDemSplitWorker()
+        {
+            //Arrange
+            string inputString = "Charlotte Clein 1500 9";
+            int arrayCountExpected = 4;
+
+            //Act
+            var stringManager = new StringManager(inputString);
+
+            //Assert
+            Assert.AreEqual(arrayCountExpected, stringManager.SplitString().Length);
+        }
+    }
+}
